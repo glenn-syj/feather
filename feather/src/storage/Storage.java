3f18e99b3d0f3ee666d8c1a545de5645dc0da755
glenn-syj <glenn.syj@gmail.com>
2025-02-17 23:34:52 +0900

    modify abstract storage layer for multiple implementations
    - Remove Path field from Storage class to support non-filesystem storage
    - Add listFiles() method for segment management and further recovery

--- feather/src/storage/Storage.java
+++ feather/src/storage/Storage.java
@@ -5,15 +5,9 @@ import storage.file.SegmentFile;
 
 import java.io.Closeable;
 import java.io.IOException;
-import java.nio.file.Path;
 
 public abstract class Storage implements Closeable {
-    protected final Path rootPath;
     private volatile boolean closed = false;
-
-    protected Storage(Path rootPath) {
-        this.rootPath = rootPath;
-    }
 
     protected final void ensureOpen() {
         if (closed) {
@@ -21,12 +15,12 @@ public abstract class Storage implements Closeable {
         }
     }
 
-    // segment files should not be modified
-    public abstract SegmentFile createFile(String name, FileType type)
-            throws IOException;
+    // file operations
+    public abstract SegmentFile createFile(String name, FileType type) throws IOException;
     public abstract SegmentFile openFile(String name) throws IOException;
     public abstract void deleteFile(String name) throws IOException;
     public abstract boolean exists(String name);
+    public abstract String[] listFiles() throws IOException;
 
     @Override
     public void close() throws IOException {
@@ -39,7 +33,5 @@ public abstract class Storage implements Closeable {
         }
     }
 
-    protected void closeInternal() throws IOException {
-        // 하위 클래스에서 구현
-    }
+    protected abstract void closeInternal() throws IOException;
 }

1338988996092717892539a06f9b1d66c5ea23d4
glenn-syj <glenn.syj@gmail.com>
2025-11-04 21:41:59 +0900

    refactor LuceneAnalyzerAdapter to covert Lucene TokenStream into Stream<FeatherToken>

--- feather/src/core/analysis/LuceneAnalyzerAdapter.java
+++ feather/src/core/analysis/LuceneAnalyzerAdapter.java
@@ -6,8 +6,9 @@ import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.UncheckedIOException;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 public class LuceneAnalyzerAdapter implements FeatherAnalyzer {
 
@@ -18,20 +19,35 @@ public class LuceneAnalyzerAdapter implements FeatherAnalyzer {
     }
 
     @Override
-    public List<FeatherToken> analyze(String text) {
-        List<FeatherToken> tokens = new ArrayList<>();
-        try (TokenStream stream = luceneAnalyzer.tokenStream(null, text)) {
-            CharTermAttribute termAttr = stream.addAttribute(CharTermAttribute.class);
-            OffsetAttribute offsetAttr = stream.addAttribute(OffsetAttribute.class);
-            stream.reset();
-            while (stream.incrementToken()) {
-                tokens.add(new FeatherToken(termAttr.toString(), offsetAttr.startOffset(), offsetAttr.endOffset()));
-            }
-            stream.end();
+    public Stream<FeatherToken> analyze(String text) {
+        try {
+            TokenStream tokenStream = luceneAnalyzer.tokenStream(null, text);
+            tokenStream.reset();
+
+            CharTermAttribute termAttr = tokenStream.getAttribute(CharTermAttribute.class);
+            OffsetAttribute offsetAttr = tokenStream.getAttribute(OffsetAttribute.class);
+
+            return Stream.generate(() -> {
+                        try {
+                            if (tokenStream.incrementToken()) {
+                                return new FeatherToken(termAttr.toString(), offsetAttr.startOffset(), offsetAttr.endOffset());
+                            }
+                            return null;
+                        } catch (IOException e) {
+                            throw new UncheckedIOException(e);
+                        }
+                    })
+                    .takeWhile(Objects::nonNull)
+                    .onClose(() -> {
+                        try {
+                            tokenStream.end();
+                            tokenStream.close();
+                        } catch (IOException e) {
+                            throw new UncheckedIOException(e);
+                        }
+                    });
         } catch (IOException e) {
-            throw new RuntimeException("Lucene analysis failed", e);
+            throw new UncheckedIOException("Failed to create TokenStream from analyzer", e);
         }
-        return tokens;
     }
 }
-
